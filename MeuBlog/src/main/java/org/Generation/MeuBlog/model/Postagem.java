9caa1ac38df038b7011e3efc258a492ae714c321
Mauricio Rodrigues <mauricio.rodriguesbd@gmail.com>
2020-11-17 10:06:43 -0300

    Atualizado 17/11/2020

--- MeuBlog/src/main/java/org/Generation/MeuBlog/model/Postagem.java
+++ MeuBlog/src/main/java/org/Generation/MeuBlog/model/Postagem.java
@@ -6,86 +6,89 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
- 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table(name = "Postagem_2")
+@Table(name = "Postagem")
 public class Postagem {
 
 	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String titulo;
 
-	
 	public String getTitulo() {
 		return titulo;
 	}
-
 
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
 
-
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String nome;
-	
+
 	@NotNull
 	@Size(min = 10, max = 100)
 	private String texto;
-		
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
 
+	
+	@ManyToOne
+	@JsonIgnoreProperties("Postagem")
+	private Tema tema;
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 	public long getId() {
 		return id;
 	}
 
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 
 	public String getTexto() {
 		return texto;
 	}
 
-
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
-
 
 	public Date getDate() {
 		return date;
 	}
 
-
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
-	
+
 }
